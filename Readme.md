c4ecd84572ece30ac0233cfd3976e6acf14ac0ef
sjoedwards <samuel.j.o.edwards@gmail.com>
2021-01-15 14:24:56 +0000

    WIP no nginx

--- Readme.md
+++ Readme.md
@@ -1,10 +1,60 @@
 # Terraform
 
-- init terraform
+Following [this link](https://medium.com/avmconsulting-blog/how-to-deploy-a-dockerised-node-js-application-on-aws-ecs-with-terraform-3e6bceb48785)
+
+### Create Docker Registries
+
+- terraform init
   `terraform init`
 
-- apply terraform
+- terraform apply
   `terraform apply`
 
-- destroy terraform
-  `terraform destroy`
+Go to Amazon ECR to ensure the repos are there
+
+### Login to docker
+
+Go into AWS -> ECR and click on the repo -> push commands, follow the login command
+
+### Build image
+
+`docker build -t sjoedwards-ecr-demo .`
+
+### Tag image
+
+```
+docker tag sjoedwards-ecr-demo:latest 261905387697.dkr.ecr.eu-west-2.amazonaws.com/sjoedwards-ecr-demo:latest
+```
+
+### Push image
+
+```
+docker push 261905387697.dkr.ecr.eu-west-2.amazonaws.com/sjoedwards-ecr-demo:latest
+```
+
+Check ECR to ensure pushed successfully
+
+### Configure AWS
+
+- AWS configure
+
+- terraform init
+  `terraform init`
+
+- terraform apply
+  `terraform apply`
+
+## Access Resource
+
+On AWS -> Load Balancer -> DNS name
+
+## Key links on AWS
+
+- ECR to view containers
+- ECS to view cluster
+- VPC to view virtual private cluster
+- EC2 -> Load balancer to view load balancing
+
+## Destroy the cluster
+
+`terraform destroy`
--- main.tf
+++ main.tf
@@ -1,23 +1,183 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "terraform-providers/docker"
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-west-2" # Setting my region to London.
+}
+
+
+# Create ECR Repo
+resource "aws_ecr_repository" "my_first_ecr_repo" {
+  name = "sjoedwards-ecr-demo" # Naming my repository
+}
+
+# Create ECS Cluster
+resource "aws_ecs_cluster" "sjoedwards_cluster" {
+  name = "sjoedwards_cluster" # Naming the cluster
+}
+
+
+# Task to create the pod
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                   = "my-first-task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "my-first-task",
+      "image": "${aws_ecr_repository.my_first_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+# Role defined to execute the task
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+#Policy document pulled in from AWS
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
+
+# Service creation - this runs the task
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"                        # Naming our first service
+  cluster         = aws_ecs_cluster.sjoedwards_cluster.id     # Referencing our created Cluster
+  task_definition = aws_ecs_task_definition.my_first_task.arn # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+
+  # Need to extend our service to include a network configuration
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true                                                # Providing our containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group - this group can only accept traffic from members of the load_balancer_secuity_group
+  }
+
+  // Connecting our VPCluster to our target-group
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
+    container_name   = aws_ecs_task_definition.my_first_task.family
+    container_port   = 3000 # Specifying the container port
+  }
+}
+
+# This group can only accept traffic from members of the load_balancer_security_group
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "eu-west-2c"
+}
+
+
+
+
+
+# Load balancer config
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80 # Allowing traffic in from port 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+# Target group is going to take the trafic from the listener and forward it onto the VPC at the specified path
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id # Referencing the default VPC - the service then points to it!
+  health_check {
+    matcher = "200,301,302"
+    path    = "/"
+  }
+}
+
+# Listener is going to forward on traffic to your target group from the load balancer on the specified port
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
   }
 }
